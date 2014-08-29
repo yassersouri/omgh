8bbc1078aedb7f70aad4e827ae902c9f260cb225
yassersouri <yassersouri@gmail.com>
2014-08-29 12:56:35 +0430

    more sophisticated sample

--- src/sample.py
+++ src/sample.py
@@ -2,33 +2,53 @@ from dataset import PASCAL_VOC_2006
 from storage import datastore
 from extractor import SIFT_SIFT_Extractor
 from transforms import PCA_Transform, GMMUniversalVocabulary
+from datatime import datatime as dt
 
-COMPLETE = True
+
+COMPLETE = False
+CALC_FEATURES = True
+CALC_PCA_TRANSFORMS = True
 
 voc2006 = PASCAL_VOC_2006(
     '/Users/yasser/sharif-repo/Datasets/VOCdevkit/VOC2006')
-temp_storage = datastore('/Users/yasser/datastores/toobreh/exp1')
-feature_extractor = SIFT_SIFT_Extractor(temp_storage)
+features_storage = datastore('/Users/yasser/datastores/toobreh/features')
+feature_extractor = SIFT_SIFT_Extractor(features_storage)
 
-if COMPLETE:
+if COMPLETE or CALC_FEATURES:
+    a = dt.now()
     for t, des in feature_extractor.extract(voc2006, 'train'):
         print t['img_id']
     for t, des in feature_extractor.extract(voc2006, 'test'):
         print t['img_id']
+    b = dt.now()
+    print 'extracting and saving all features: \t', (b - a)
 
-pca_t = PCA_Transform(temp_storage, 50)
+transforms_storage = datastore(
+    '/Users/yasser/datastores/toobreh/exp1/transforms')
+pca_t = PCA_Transform(transforms_storage, 50)
+a = dt.now()
 pca_t.fit(feature_extractor.extract(voc2006, 'train'))
+b = dt.now()
+print 'fitting pca: \t', (b - a)
 
-if COMPLETE:
+
+if COMPLETE or CALC_PCA_TRANSFORMS:
+    a = dt.now()
     for t, des in pca_t.transform(feature_extractor.extract(voc2006, 'train')):
         print t['img_id']
     for t, des in pca_t.transform(feature_extractor.extract(voc2006, 'test')):
         print t['img_id']
+    b = dt.now()
+    print 'calculating all pcas: \t', (b - a)
 
 uni_vocab = GMMUniversalVocabulary(
-    temp_storage, n_components=128, covariance_type='diag')
+    transforms_storage, n_components=1, covariance_type='diag',
+    n_iter=1, n_init=1)
+a = dt.now()
 uni_vocab.fit(
     pca_t.transform(feature_extractor.extract(voc2006, 'train')), test=True)
+b = dt.now()
+print 'fitting gmm: \t', (b - a)
 
 if COMPLETE:
     for t, des in uni_vocab.transform(pca_t.transform(
