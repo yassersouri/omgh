6ae5aa583f2c39e72137eef08d97095125167fd6
Yasser Souri <yassersouri@gmail.com>
2014-11-15 10:55:33 +0330

    some cleanup and update

--- src/deep_extractor.py
+++ src/deep_extractor.py
@@ -1,13 +1,13 @@
 from extractor import BaseExtractor
 import numpy as np
 import sys
-sys.path.append('/home/ipl/installs/caffe-0.9999/python/')
+sys.path.append('/home/ipl/installs/caffe-rc/python/')
 import caffe
 
 
 class CNN_Features_CAFFE_REFERENCE(BaseExtractor):
 
-    def __init__(self, storage, model_file, pretrained_file, ilsvrc_mean):
+    def __init__(self, storage, model_file, pretrained_file, image_mean):
         super(CNN_Features_CAFFE_REFERENCE, self).__init__(storage)
         self.STORAGE_SUB_NAME = 'cnn_feature_caffe_reference'
         self.feature_layer = 'fc7'
@@ -19,15 +19,15 @@ class CNN_Features_CAFFE_REFERENCE(BaseExtractor):
 
         self.model_file = model_file
         self.pretrained_file = pretrained_file
-        self.ilsvrc_mean = ilsvrc_mean
+        self.image_mean = image_mean
 
         self.net = caffe.Classifier(self.model_file,
-                                    self.pretrained_file)
-        self.net.set_phase_test()
-        self.net.set_mode_cpu()
-        self.net.set_mean('data', np.load(self.ilsvrc_mean))
-        self.net.set_raw_scale('data', 255)
-        self.net.set_channel_swap('data', (2, 1, 0))
+                                    self.pretrained_file,
+                                    mean=np.load(self.image_mean),
+                                    channel_swap=(2,1,0),
+                                    raw_scale=255,
+                                    image_dims=(256, 256))
+        self.net.set_mode_gpu()
 
     def extract(self, data_generator, force=False):
         for t in data_generator:
@@ -55,9 +55,11 @@ class CNN_Features_CAFFE_REFERENCE(BaseExtractor):
             instance_path = self.storage.get_instance_path(
                 self.STORAGE_SUPER_NAME, self.STORAGE_SUB_NAME, instance_name)
             if force or not self.storage.check_exists(instance_path):
+                #TODO: move to sepatate funciton
                 im_cropped = caffe.io.load_image(t['img_file'])
                 x,y,w,h = bbox[int(t['img_id'])-1]
                 im_cropped = im_cropped[y:y+h,x:x+w]
+
                 self.net.predict([im_cropped])
 
                 des = self.net.blobs[self.feature_layer].data[
@@ -71,3 +73,15 @@ class CNN_Features_CAFFE_REFERENCE(BaseExtractor):
 
             yield t, des
 
+
+    def extract_one(self, img_id):
+        instance_name = "%s.%s" % (img_id, self.FILE_NAMES_EXT)
+        instance_path = self.storage.get_instance_path(
+            self.STORAGE_SUPER_NAME, self.STORAGE_SUB_NAME, instance_name)
+        if not self.storage.check_exists(instance_path):
+            raise Exception("Calculate deep features first then load them.")
+        else:
+            des = self.storage.load_instance(instance_path)
+            if len(des.shape) > 1:
+                des = des[0, :]
+            return des
--- src/sample_cub_caffe_cropped.py
+++ src/sample_cub_caffe_cropped.py
@@ -6,9 +6,9 @@ from deep_extractor import CNN_Features_CAFFE_REFERENCE
 cub = CUB_200_2011(
     '/home/ipl/datasets/CUB-200-2011/CUB_200_2011/CUB_200_2011')
 features_storage = datastore('/home/ipl/datastores/cub-caffe-features-cropped/')
-model_file = '/home/ipl/installs/caffe-0.9999/examples/imagenet/imagenet_deploy.prototxt'
-pretrained_file = '/home/ipl/installs/caffe-0.9999/examples/imagenet/caffe_reference_imagenet_model'
-ilsvrc_mean = '/home/ipl/installs/caffe-0.9999/python/caffe/imagenet/ilsvrc_2012_mean.npy'
+model_file = '/home/ipl/installs/caffe-rc/models/bvlc_reference_caffenet/deploy.prototxt'
+pretrained_file = '/home/ipl/installs/caffe-rc/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+ilsvrc_mean = '/home/ipl/installs/caffe-rc/python/caffe/imagenet/ilsvrc_2012_mean.npy'
 feature_extractor = CNN_Features_CAFFE_REFERENCE(features_storage,
     model_file, pretrained_file, ilsvrc_mean)
 
