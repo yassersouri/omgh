68b455183d6077684d86547c485a0219d52da3b4
Yasser Souri <yassersouri@gmail.com>
2014-12-22 10:45:57 +0330

    stuff to compute head part features

--- src/settings.py
+++ src/settings.py
@@ -49,6 +49,7 @@ dyn_aug('cccftv1_2', 'finetune_cub_cropped_val_1_2', 'finetune_cub', 10)
 dyn_aug('cccftv2', 'finetune_cub_cropped_val_2', 'finetune_cub', 10)
 dyn_aug('cccftt', 'finetune_cub_cropped', 'finetune_cub_cropped', 10)
 dyn_aug('ccrftt', 'finetune_cub', 'finetune_cub', 10)
+dyn_aug('ccpheadft', 'finetune_cub_part_head', 'finetune_cub_part_head', 10)
 
 
 def storage(sname):
