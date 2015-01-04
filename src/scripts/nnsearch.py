fc07f424331b8f1cfcd38f213340230c9d5b1ecd
Yasser Souri <yassersouri@gmail.com>
2015-01-05 01:06:33 +0330

    implement basic nn search

--- src/scripts/nnsearch.py
+++ src/scripts/nnsearch.py
@@ -2,13 +2,12 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import numpy as np
-sys.path.append('/home/ipl/installs/caffe-rc/python/')
-import caffe
 import settings
-import utils
 from storage import datastore
 from dataset import CUB_200_2011
 import click
+import sklearn.neighbors
+from time import time
 
 
 @click.command()
@@ -16,6 +15,11 @@ def main():
     storage_name = 'cache-cccftt'
     layer = 'pool5'
     name = '%s-%s' % ('cccftt', 100000)
+    normalize_feat = True
+    n_neighbors = 1
+
+    A = 100
+    N = 100
 
     cub = CUB_200_2011(settings.CUB_ROOT)
 
@@ -25,7 +29,37 @@ def main():
 
     instance_path = safe.get_instance_path(safe.super_name, safe.sub_name, 'feat_cache_%s' % layer)
     feat = safe.load_large_instance(instance_path, 4)
-    print feat.shape
+
+    # should we normalize the feats?
+    if normalize_feat:
+        # snippit from : http://stackoverflow.com/a/8904762/428321
+        row_sums = feat.sum(axis=1)
+        new_feat = feat / row_sums[:, np.newaxis]
+        feat = new_feat
+
+    IDtrain, IDtest = cub.get_train_test_id()
+    # bbox = cub.get_bbox()
+    # parts = cub.get_parts()
+
+    # the following line is not really a good idea. Only works for this dataset.
+    Xtrain = feat[IDtrain-1, :]
+    Xtest = feat[IDtest-1, :]
+
+    nn_model = sklearn.neighbors.NearestNeighbors(n_neighbors=n_neighbors, algorithm='ball_tree', metric='minkowski', p=2)
+    tic = time()
+    nn_model.fit(Xtrain)
+    toc = time() - tic
+    print 'fitted in: ', toc
+
+    print 'test ids'
+    print ', '.join([str(x) for x in IDtest[A:A+N]])
+
+    tic = time()
+    NNS = nn_model.kneighbors(Xtest[A:A+N], 1, return_distance=False).T
+    toc = time() - tic
+    print 'found in: ', toc
+    print 'train ids'
+    print ', '.join([str(x) for x in NNS[0]])
 
 
 if __name__ == '__main__':
