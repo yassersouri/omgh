a70bc53b69a6c42d7730a96dccfefbca0f22f4c9
Yasser Souri <yassersouri@gmail.com>
2015-01-07 02:01:40 +0330

    a script to calculate them all

---
+++ src/do_all_nn.sh
@@ -0,0 +1,57 @@
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise False
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 1 --noise_std_d 1
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 1 --noise_std_d 1
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 1 --noise_std_d 1
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 1 --noise_std_d 1
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 1 --noise_std_d 1
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 5 --noise_std_d 5
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 5 --noise_std_d 5
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 5 --noise_std_d 5
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 5 --noise_std_d 5
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 5 --noise_std_d 5
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 10 --noise_std_d 10
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 10 --noise_std_d 10
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 10 --noise_std_d 10
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 10 --noise_std_d 10
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 10 --noise_std_d 10
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 15 --noise_std_d 15
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 15 --noise_std_d 15
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 15 --noise_std_d 15
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 15 --noise_std_d 15
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 15 --noise_std_d 15
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 20 --noise_std_d 20
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 20 --noise_std_d 20
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 20 --noise_std_d 20
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 20 --noise_std_d 20
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle True --add_noise True --noise_std_c 20 --noise_std_d 20
+
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer pool5 --normalize_feat True
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer pool5 --normalize_feat False
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer conv5 --normalize_feat True
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer conv5 --normalize_feat False
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer conv4 --normalize_feat True
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer conv4 --normalize_feat False
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer conv3 --normalize_feat True
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer conv3 --normalize_feat False
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer fc6 --normalize_feat True
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer fc6 --normalize_feat False
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer fc7 --normalize_feat True
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --to_oracle False --add_noise False --layer fc7 --normalize_feat False
+
