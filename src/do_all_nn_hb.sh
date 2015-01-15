87f0344c8c9638c1120f5bd2a33598e752bbd6e0
Yasser Souri <yassersouri@gmail.com>
2015-01-16 02:06:43 +0330

    COMPUTE ALL THE THINGS!

---
+++ src/do_all_nn_hb.sh
@@ -0,0 +1,31 @@
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 1 --augmentation_noise 5
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 1 --augmentation_noise 10
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 1 --augmentation_noise 20
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 1 --augmentation_noise 40
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 2 --augmentation_noise 5
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 3 --augmentation_noise 5
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 4 --augmentation_noise 5
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 2 --augmentation_noise 10
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 3 --augmentation_noise 10
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 4 --augmentation_noise 10
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 2 --augmentation_noise 20
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 3 --augmentation_noise 20
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 4 --augmentation_noise 20
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 2 --augmentation_noise 40
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 3 --augmentation_noise 40
+
+GLOG_minloglevel=10 python scripts/nnsearch.py --parts head --parts body --to_oracle False --add_noise False --augment_training True --augmentation_fold 4 --augmentation_noise 40
