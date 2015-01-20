321a9f297f1a3b9f6f6ef8be137cd412903b3616
Yasser Souri <yassersouri@gmail.com>
2015-01-20 19:01:36 +0330

    compute features for RF

--- src/cub_utils.py
+++ src/cub_utils.py
@@ -20,11 +20,11 @@ def thresh_segment_mean(seg):
 
 
 def gen_part_points(part_rect, seg, parts):
-    N = 100
+    N = 10
     xmin, xmax, ymin, ymax = part_rect
 
-    xs = np.random.uniform(low=xmin, high=xmax, size=N)
-    ys = np.random.uniform(low=ymin, high=ymax, size=N)
+    xs = np.random.uniform(low=xmin+1, high=xmax-1, size=N)
+    ys = np.random.uniform(low=ymin+1, high=ymax-1, size=N)
 
     parts = Parts()
 
@@ -35,12 +35,12 @@ def gen_part_points(part_rect, seg, parts):
 
 
 def gen_bg_points(part_rect, seg, parts):
-    N = 500
+    N = 100
     h, w = seg.shape[0], seg.shape[1]
     xmin, xmax, ymin, ymax = part_rect
 
-    xs = np.random.uniform(low=0, high=h-1, size=N)
-    ys = np.random.uniform(low=0, high=w-1, size=N)
+    xs = np.random.uniform(low=1, high=h, size=N)
+    ys = np.random.uniform(low=1, high=w, size=N)
 
     parts = Parts()
 
@@ -93,3 +93,54 @@ class DeepHelper(object):
 
             self.num_feats[layer] = num_feat
             self.feats[layer] = data
+
+    def features(self, points, layers=None):
+        n_points = len(points)
+        if layers is None:
+            layers = self.layers
+        n_features = sum(self.num_feats[l] for l in layers)
+        features = np.zeros((n_points, n_features))
+
+        for i, point in enumerate(points):
+            x, y = point.y - 1, point.x - 1  # not because I'm idoit, but because of other things!
+            feat_layers = [self.feats[l][x, y, :] for l in layers]
+            features[i, :] = np.concatenate(feat_layers)
+
+        return features
+
+    def part_for_image(self, all_image_infos, all_segmentaion_infos, cub_parts, img_id, part_filter_names):
+        img = caffe.io.load_image(all_image_infos[img_id])
+        seg = thresh_segment_mean(caffe.io.load_image(all_segmentaion_infos[img_id]))
+
+        self.init_with_image(img)
+
+        parts = cub_parts.for_image(img_id)
+        part_parts = parts.filter_by_name(part_filter_names)
+        part_positive = gen_part_points(part_parts.get_rect_info(img), seg, part_parts)
+        part_negative = gen_bg_points(part_parts.get_rect_info(img), seg, part_parts)
+
+        part_positive.norm_for_size(img.shape[1], img.shape[0], self.input_dim)
+        part_negative.norm_for_size(img.shape[1], img.shape[0], self.input_dim)
+
+        feats_positive = self.features(part_positive)
+        feats_negative = self.features(part_negative)
+
+        return feats_positive, feats_negative
+
+    def part_features_for_rf(self, all_image_infos, all_segmentaion_infos, cub_parts, IDs, part_filter_names):
+        positives = []
+        negatives = []
+        for i, img_id in enumerate(IDs):
+            feats_positive, feats_negative = self.part_for_image(all_image_infos, all_segmentaion_infos, cub_parts, img_id, part_filter_names)
+
+            positives.append(feats_positive)
+            negatives.append(feats_negative)
+        X_pos = np.vstack(positives)
+        y_pos = np.ones((X_pos.shape[0]), np.int)
+        X_neg = np.vstack(negatives)
+        y_neg = np.zeros((X_neg.shape[0]), np.int)
+
+        X = np.vstack((X_pos, X_neg))
+        y = np.concatenate((y_pos, y_neg))
+
+        return X, y
