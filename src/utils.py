d4b49aa1cdeb9b3d47b719d3a1fecb580b1eb580
Yasser Souri <yassersouri@gmail.com>
2015-02-03 16:10:25 +0330

    better draw_bbox function

--- src/utils.py
+++ src/utils.py
@@ -15,8 +15,11 @@ def ensure_dir(address):
         os.makedirs(address)
 
 
-def draw_bbox(img, bbox):
-        bx, by, bw, bh = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+def draw_bbox(img, bbox, color=100, width=2):
+        try:
+            bx, by, bw, bh = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+        except:
+            bx, by, bw, bh = bbox
         new_img = img.copy()
-        cv2.rectangle(new_img, (bx, by), (bx+bw, by+bh), 100, 2)
+        cv2.rectangle(new_img, (bx, by), (bx+bw, by+bh), color, width)
         return new_img
